bb5f8162d96184ce26d0296ad3eabad190fd2ff3
souvik4391 <souvik.manutd@gmail.com>
2020-08-05 00:56:10 +0530

    Initial Check-in 1

---
+++ src/main/java/com/example/msscbreweryclient/msscBreweryClient/MSSCBreweryClientApplication.java
@@ -0,0 +1,13 @@
+package com.example.msscbreweryclient.msscBreweryClient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MSSCBreweryClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MSSCBreweryClientApplication.class, args);
+    }
+
+}
